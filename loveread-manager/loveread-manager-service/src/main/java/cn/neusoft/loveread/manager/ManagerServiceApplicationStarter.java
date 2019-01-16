49172e3f35e0c30ba026ea59781a329fd2a2c235
limingxuan <minghan.idward@gmail.com>
2019-01-16 20:43:31 +0800

    pagehelper dubbo clash

--- loveread-manager/loveread-manager-service/src/main/java/cn/neusoft/loveread/manager/ManagerServiceApplicationStarter.java
+++ loveread-manager/loveread-manager-service/src/main/java/cn/neusoft/loveread/manager/ManagerServiceApplicationStarter.java
@@ -2,12 +2,22 @@ package cn.neusoft.loveread.manager;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.concurrent.CountDownLatch;
 
 @MapperScan(value = "cn.neusoft.loveread.manager.mapper")
 @SpringBootApplication
 public class ManagerServiceApplicationStarter {
-    public static void main(String[] args) {
-        SpringApplication.run(ManagerServiceApplicationStarter.class, args);
+    public static void main(String[] args) throws InterruptedException {
+//        SpringApplication.run(ManagerServiceApplicationStarter.class, args);
+        new SpringApplicationBuilder()
+                .sources(ManagerServiceApplicationStarter.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.await();
     }
 }
